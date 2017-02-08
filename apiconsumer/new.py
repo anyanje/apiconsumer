9aef291be68fc8418ceb23a7993642708e1648d0
unknown <loretaanyanje@gmail.com>
2017-02-08 22:36:07 +0300

    first commit

---
+++ apiconsumer/new.py
@@ -0,0 +1,14 @@
+class client_library(public):
+"""user request initialization"""
+def __init__(self,client_library,):
+self.client_library=client_library
+client_library=public_Api
+
+"""creating instances on the client _library"""
+if self.client_library != "request":
+return"request rquired"
+elif self.client_library == "request":
+url:'http://api.github.com/repos/request/request'
+return "User_Agent"
+else:
+return "server error"
